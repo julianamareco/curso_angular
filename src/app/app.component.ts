853a3919a85d044429ad8bd5cea7485727e87752
Juliana Mareco <julianamareco18@gmail.com>
2020-04-15 13:19:39 -0300

    Aula 3: Integração com Web API's

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  photos = [
-    {
-      url: "https://www.petz.com.br/blog/wp-content/uploads/2019/07/vida-de-gato.jpg", 
-      description: "Gato"
-    },
-    {
-      url:"https://abrilexame.files.wordpress.com/2019/09/gettyimages-908714708.gif",
-      description:"Gato Preto"
-    }
-  ];
+export class AppComponent implements OnInit{
+  photos: any[] = [];
+
+  constructor(private phoyoService: PhotoService) {}
+
+  // const observable = http.get('http://localhost:3000/flavio/photos');
+  // observable.subscribe();
+
+  ngOnInit(): void {
+    this.phoyoService
+      .listFromUser('flavio')
+      .subscribe(photos => {
+        console.log(photos[0].userId);
+        this.photos = photos
+      });
+
+  }
 
 }
