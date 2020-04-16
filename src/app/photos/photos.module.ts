b82572454d425972d77a601eb485127248f21bc6
Juliana Mareco <julianamareco18@gmail.com>
2020-04-15 23:27:37 -0300

    Aula 6: Melhorando a experiência do usuário

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -6,6 +6,8 @@ import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
+import { FilterByDescription } from './photo-list/filter-by-description.pipe';
+import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
 
 //Boa prática importar em todas as modules criadas o CommonModulo para tornar disponível as diretivas do angular
 
@@ -14,7 +16,9 @@ import { PhotosComponent } from './photo-list/photos/photos.component';
         PhotoComponent,
         PhotoListComponent,
         PhotoFormComponent,
-        PhotosComponent
+        PhotosComponent,
+        FilterByDescription,
+        LoadButtonComponent
     ],
     imports: [
         HttpClientModule,
